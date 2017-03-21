dc837d6cd4817a553255ccdd850125cdfe8519f9
Tony Wall <tony@tonywall.co>
2017-03-21 04:02:08 +0100

    Rename part 1

--- Framework/Emlid.WindowsIoT.Hardware/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-// Information
-[assembly: AssemblyTitle("Emlid.WindowsIot.Hardware")]
-[assembly: AssemblyDescription("Emlid Windows IoT Hardware SDK")]
-
-// Components should be CLS compliant
-[assembly: CLSCompliant(true)]
